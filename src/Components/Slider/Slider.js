0ab8234c71483edf70bbc9a6f016e5e41ff8bcae
Richard Rinaldi <rinaldi.richard@gmail.com>
2021-10-19 07:53:53 +0200

    Add btnSlider

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./Slider.css";
 import dataSlider from "./dataSlider";
+import BtnSlider from "./BtnSlider";
 
 export default function Slider() {
   // useState sera un objet car besoin de 2 propriétés
@@ -8,6 +9,26 @@ export default function Slider() {
     index: 1,
     inProgress: false,
   });
+  const nextSlide = () => {
+    // Si l'index est strictement différent de la longeur du tableau de dataSlider
+    if (slideAnim.index !== dataSlider.length) {
+
+      setSlideAnim({ index: slideAnim.index + 1, 
+      inProgress: true });
+
+    } 
+    
+    else if (slideAnim.index === dataSlider.length) {
+
+      setSlideAnim({ index: 1, inProgress: true });
+
+    }
+  };
+  const prevSlide = () => {
+     if (slideAnim.index !== dataSlider.length) {
+       setSlideAnim({ index: slideAnim.index - 1, inProgress: true });
+     }
+  };
 
   return (
     <div className="container-slider">
@@ -19,14 +40,20 @@ export default function Slider() {
             key={obj.id}
             // index = 0 dans avec le map et démarre a 1 via useState donc 0+1...
             className={
-            // si index =1 on lui donne la classe correspondante sinon l'autre classe    
+              // si index =1 on lui donne la classe correspondante sinon l'autre classe
               slideAnim.index === index + 1 ? "slide active-anim" : "slide"
             }
           >
-              <img src={process.env.PUBLIC_URL+`/Imgs/img${index+1}.jpg`} alt="" />
+            <img
+              src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`}
+              alt=""
+            />
           </div>
         );
       })}
+
+      <BtnSlider moveSlide={nextSlide} direction={"next"} />
+      <BtnSlider moveSlide={prevSlide} direction={"prev"} />
     </div>
   );
 }
